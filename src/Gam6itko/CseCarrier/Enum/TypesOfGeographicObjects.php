d2cdb4ba0a3b5a4234d8710438ff224f724cf794
gambit <gam6itko@gmail.com>
2017-08-17 11:48:17 +0300

    TypesOfGeographicObjects

---
+++ src/Gam6itko/CseCarrier/Enum/TypesOfGeographicObjects.php
@@ -0,0 +1,13 @@
+<?php
+namespace Gam6itko\CseCarrier\Enum;
+
+final class TypesOfGeographicObjects
+{
+    const Country = 0;
+    const Region = 1;
+    const District = 2;
+    const City = 3;
+    const Town = 4;
+    const Street = 5;
+    const Other = 6;
+}
